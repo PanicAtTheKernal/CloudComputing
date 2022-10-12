3aa4346e63055a0f36da3c1bfad1d405614e5405
Daniel Kondabarov <drk3nlzux@relay.firefox.com>
2022-10-12 15:42:45 +0100

    More modifed code

--- app/src/static/js/app.js
+++ app/src/static/js/app.js
@@ -5,6 +5,7 @@ function App() {
             <Row>
                 <Col md={{ offset: 3, span: 6 }}>
                     <TodoListCard />
+                    <ArchiveListCard />
                 </Col>
             </Row>
         </Container>
@@ -52,6 +53,62 @@ function TodoListCard() {
     return (
         <React.Fragment>
             <AddItemForm onNewItem={onNewItem} />
+            {items.length === 0 && (
+                <p className="text-center litext">You have no todo items yet! Add one above or not ;)</p>
+            )}
+            {items.map(item => (
+                <ItemDisplay
+                    item={item}
+                    key={item.id}
+                    onItemUpdate={onItemUpdate}
+                    onItemRemoval={onItemRemoval}
+                />
+            ))}
+        </React.Fragment>
+    );
+}
+
+function ArchiveListCard() {
+    const [items, setItems] = React.useState(null);
+
+    React.useEffect(() => {
+        fetch('/items')
+            .then(r => r.json())
+            .then(setItems);
+    }, []);
+
+    const onNewItem = React.useCallback(
+        newItem => {
+            setItems([...items, newItem]);
+        },
+        [items],
+    );
+
+    const onItemUpdate = React.useCallback(
+        item => {
+            const index = items.findIndex(i => i.id === item.id);
+            setItems([
+                ...items.slice(0, index),
+                item,
+                ...items.slice(index + 1),
+            ]);
+        },
+        [items],
+    );
+
+    const onItemRemoval = React.useCallback(
+        item => {
+            const index = items.findIndex(i => i.id === item.id);
+            setItems([...items.slice(0, index), ...items.slice(index + 1)]);
+        },
+        [items],
+    );
+
+    if (items === null) return 'Loading...';
+
+    return (
+        <React.Fragment>
+            <h1>Archived Cards</h1>
             {items.length === 0 && (
                 <p className="text-center litext">You have no todo items yet! Add one above or not ;)</p>
             )}
