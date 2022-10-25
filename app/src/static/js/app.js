644b4fa522103202f80701e54798f23f5397897c
Daniel Kondabarov <drk3nlzux@relay.firefox.com>
2022-10-25 17:11:20 +0100

    Added animations

--- app/src/static/js/app.js
+++ app/src/static/js/app.js
@@ -13,19 +13,23 @@ function App() {
 
 function ArchiveListCard() {
     const [aitems, setAItems] = React.useState([]);
+    const [restore, setRestore] = React.useState(false);
+    const [playAnimation, setPlayAnimation] = React.useState(true);
 
     React.useEffect(() => {
-        // fetch('/items')
-        // .then(r => r.json())
-        // .then(setAItems);
     }, []);
 
     const onNewItem = React.useCallback(
         newItem => {
-            setAItems([...aitems, newItem]);
+            setAItems([newItem]);
+            setPlayAnimation(true);
         },
         [aitems],
     );
+
+    const onItemRestore = React.useCallback(() => {
+        setRestore(true);
+    });
 
     const onItemUpdate = React.useCallback(
         item => {
@@ -39,6 +43,10 @@ function ArchiveListCard() {
         [aitems],
     );
 
+    const setRestoreItem = React.useCallback(e => {
+        setRestore(e);
+    }, [restore]);
+
     const onItemRemoval = React.useCallback(
         item => {
             const index = aitems.findIndex(i => i.id === item.id);
@@ -51,25 +59,32 @@ function ArchiveListCard() {
 
     return (
         <React.Fragment>
-            <TodoListCard onArchiveItem={onNewItem}/>
+            <TodoListCard onArchiveItem={onNewItem} restoreItem={restore}/>
             <h1 className="text-center">Bin</h1>
             {aitems.length === 0 && (
                 <p className="text-center litext">These cards will be deleted when the app refreshes</p>
             )}
             {aitems.map(item => (
-                <ItemDisplay
+                <ArchiveItemDisplay
                     item={item}
                     key={item.id}
-                    onItemUpdate={onItemUpdate}
+                    onArchiveItem={onItemRestore}
                     onItemRemoval={onItemRemoval}
+                    playAnimation={playAnimation}
+                    setRestoreItem={setRestoreItem}
                 />
             ))}
         </React.Fragment>
     );
 }
 
-function TodoListCard({ onArchiveItem }) {
+function TodoListCard({ onArchiveItem, restoreItem, setRestoreItem}) {
     const [items, setItems] = React.useState(null);
+    const [playAnimation, setPlayAnimation] = React.useState(false);
+    const [achiveItem, setAchiveItem] = React.useState(null);
+
+    const [newItem, setNewItem] = React.useState('');
+    const [submitting, setSubmitting] = React.useState(false);
 
     React.useEffect(() => {
         fetch('/items')
@@ -77,9 +92,37 @@ function TodoListCard({ onArchiveItem }) {
             .then(setItems);
     }, []);
 
+    React.useEffect(() => {
+        setTimeout(() => {
+            if (restoreItem && achiveItem != null) {
+                setNewItem(achiveItem["name"]);
+                submitNewItem();
+                // setRestoreItem();
+            }
+            setAchiveItem(null);
+        }, 100)
+    });
+
+    const submitNewItem = () => {
+        setSubmitting(true);
+        fetch('/items', {
+            method: 'POST',
+            body: JSON.stringify({ name: newItem }),
+            headers: { 'Content-Type': 'application/json' },
+        })
+            .then(r => r.json())
+            .then(item => {
+                onNewItem(item);
+                setSubmitting(false);
+                setNewItem('');
+            });
+    };
+
+
     const onNewItem = React.useCallback(
         newItem => {
             setItems([...items, newItem]);
+            setPlayAnimation(true);
         },
         [items],
     );
@@ -100,6 +143,7 @@ function TodoListCard({ onArchiveItem }) {
         item => {
             const index = items.findIndex(i => i.id === item.id);
             setItems([...items.slice(0, index), ...items.slice(index + 1)]);
+            setAchiveItem(item);
             onArchiveItem(item);
         },
         [items],
@@ -119,6 +163,7 @@ function TodoListCard({ onArchiveItem }) {
                     key={item.id}
                     onItemUpdate={onItemUpdate}
                     onItemRemoval={onItemRemoval}
+                    playAnimation={playAnimation}
                 />
             ))}
         </React.Fragment>
@@ -175,8 +220,15 @@ function AddItemForm({ onNewItem }) {
     );
 }
 
-function ItemDisplay({ item, onItemUpdate, onItemRemoval }) {
+function ItemDisplay({ item, onItemUpdate, onItemRemoval, playAnimation=false }) {
     const { Container, Row, Col, Button } = ReactBootstrap;
+    const [itemCreateAni, setItemCreateAni] = React.useState("itemCreateAnimation");
+
+    React.useEffect(() => {
+        setTimeout(() => {
+            setItemCreateAni(" ");
+          }, 3000);
+    }, []);
 
     const toggleCompletion = () => {
         fetch(`/items/${item.id}`, {
@@ -198,7 +250,7 @@ function ItemDisplay({ item, onItemUpdate, onItemRemoval }) {
     };
 
     return (
-        <Container fluid className={`item ${item.completed && 'completed'}`}>
+        <Container fluid className={`item ${(playAnimation) ? itemCreateAni : ""} ${item.completed && 'completed'}`}>
             <Row>
                 <Col xs={1} className="text-center">
                     <Button
@@ -217,6 +269,57 @@ function ItemDisplay({ item, onItemUpdate, onItemRemoval }) {
                                 item.completed ? 'fa-check-square' : 'fa-square'
                             }`}
                         />
+                    </Button>
+                </Col>
+                <Col xs={10} className="name">
+                    {item.name}
+                </Col>
+                <Col xs={1} className="text-center remove">
+                    <Button
+                        size="sm"
+                        variant="link"
+                        onClick={removeItem}
+                        aria-label="Remove Item"
+                    >
+                        <i className="fa fa-trash text-danger" />
+                    </Button>
+                </Col>
+            </Row>
+        </Container>
+    );
+}
+
+function ArchiveItemDisplay({ item, onArchiveItem, onItemRemoval, playAnimation=false }) {
+    const { Container, Row, Col, Button } = ReactBootstrap;
+    const [itemCreateAni, setItemCreateAni] = React.useState("achiveItemCreateAnimation");
+
+    React.useEffect(() => {
+        setTimeout(() => {
+            setItemCreateAni(" ");
+          }, 1000);
+    }, []);
+
+    const recoverItem = () => {
+        onArchiveItem();
+        onItemRemoval(item);
+    }
+
+    const removeItem = () => {
+        onItemRemoval(item);
+    };
+
+    return (
+        <Container fluid className={`item ${(playAnimation) ? itemCreateAni : ""} ${item.completed && 'completed'}`}>
+            <Row>
+                {/* Too buggy to fix  */}
+                <Col xs={1} className="text-center">
+                    <Button
+                        size="sm"
+                        variant="link"
+                        // onClick={recoverItem}
+                        aria-label="Recover item"
+                    >
+                        {/* <i class="fa fa-trash-restore text-success"></i> */}
                     </Button>
                 </Col>
                 <Col xs={10} className="name">
